c4170015cfc27e6757487b705c2e89c945c95282
Nicolas SWIGON <swigon.nicolas@gmail.com>
2022-06-28 15:56:45 +0200

    maj sql

--- projetblog.sql
+++ projetblog.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : lun. 27 juin 2022 à 16:33
--- Version du serveur :  10.4.19-MariaDB
--- Version de PHP : 8.0.6
+-- Généré le : mar. 28 juin 2022 à 15:55
+-- Version du serveur : 10.4.22-MariaDB
+-- Version de PHP : 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -37,6 +37,14 @@ CREATE TABLE `article` (
   `owner_id` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Déchargement des données de la table `article`
+--
+
+INSERT INTO `article` (`id`, `img_url`, `title`, `content`, `statut`, `category_id`, `owner_id`) VALUES
+(1, 'https://i0.wp.com/www.kimonosport.com/wp-content/uploads/2018/08/van_damme_joven.jpg?w=612&ssl=1', 'Leur lutte était constante pour devenir acteur', 'Sans doute, ce pour quoi Jean Claude Van Damme s’est le plus battu, c’est à cause de son rêve de devenir acteur. A 22 ans, le Belge vend sa salle de sport et se rend aux Etats-Unis sans papiers et sans connaître beaucoup la langue. Là, il a dû faire toutes sortes de travaux en tant que chauffeur de taxi, nettoyeur de chaussures, serveur, livreur de pizza, chauffeur de limousine, et même il prétend dans son film qu’il a continué à voler pour qu’il puisse manger.', 'public', 1, 2),
+(2, 'https://img.filmsactu.net/datas/personnes/j/e/jean-claude-van-damme/n/jean-claude-van-damme-613716305ade0.jpg', 'Bam Boom Tap', 'Quand tu fais le calcul, je suis mon meilleur modèle car entre penser et dire, il y a un monde de différence et c\'est une sensation réelle qui se produit si on veut ! Donc on n\'est jamais seul spirituellement !\r\n\r\nAh non attention, je sais que, grâce à ma propre vérité c\'est juste une question d\'awareness et ça, c\'est très dur, et, et, et... c\'est très facile en même temps. Il y a un an, je t\'aurais parlé de mes muscles.', '', 2, 5);
+
 -- --------------------------------------------------------
 
 --
@@ -45,9 +53,17 @@ CREATE TABLE `article` (
 
 CREATE TABLE `category` (
   `id` int(10) UNSIGNED NOT NULL,
-  `title` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
   `img_url` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `category`
+--
+
+INSERT INTO `category` (`id`, `title`, `img_url`) VALUES
+(1, 'philosophie', 'https://i0.wp.com/www.kimonosport.com/wp-content/uploads/2018/08/historia-de-jean-claude-van-damme-1024x575.jpg?resize=1024%2C575&ssl=1'),
+(2, 'bagarre', 'https://www.nostalgie.be/build/images/mask02.svg');
 
 -- --------------------------------------------------------
 
@@ -83,8 +99,12 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id`, `mail`, `name`, `firstname`, `birthday`, `password`, `is_admin`) VALUES
 (2, 'machin@truc.fr', 'macin', 'truc', '2012-06-13', '12345678', 0),
-(3, 'test', 'testoi', 'toi@test.fr', '2013-06-27', '$2y$10$tA9kh7tjlC3m9lWF4rojbuXN5o9yDy5ng.0ho6Y2sBSVBYACehYn6', 0),
-(4, 'van', 'jean', 'van@jc.fr', '2022-06-09', '$2y$10$Uji5RNqzBB6rZ2XV73kvK.BqAjdZcV9fPW23oz.jnNewdukSdMn42', 0);
+(3, 'toi@test.fr', 'testoi', 'test', '2013-06-27', '$2y$10$tA9kh7tjlC3m9lWF4rojbuXN5o9yDy5ng.0ho6Y2sBSVBYACehYn6', 0),
+(4, 'van@jc.fr', 'jean', 'van', '2022-06-09', '$2y$10$Uji5RNqzBB6rZ2XV73kvK.BqAjdZcV9fPW23oz.jnNewdukSdMn42', 0),
+(5, 'julien@loic.fr', 'Julien', 'truc', '2011-12-28', '$2y$10$Eem0xjCv0wddXLvt2BV5ke2kuxZWcEpRTEZZN78z8x7YT2yC0Pmai', 0),
+(6, 'fouad@loic.fr', 'Fouad', 'Loic', '2022-06-02', '$argon2id$v=19$m=65536,t=4,p=1$V1FUTnFJblhOdmtnSUwyVg$mVxK4TgqA0/pNFA6X04YcYoJPjHbii5bH6kWVzO0JvA', 0),
+(7, 'michel@michel.fr', 'michel', 'michel', '2022-06-30', '$2y$10$/T95LE0VfATmD0CVhqN3C.rTi00KRycMMwTAJnJ3jmHfE32jU95ce', 0),
+(8, 'cath@caht.fr', 'catherin', 'cath', '2022-06-09', '$2y$10$RzCQybAnvdIZjbm1mUyZCe.7RM7Zn9JF7h23BtZPkWe0lmP1uE/Y.', 0);
 
 --
 -- Index pour les tables déchargées
@@ -126,19 +146,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pour la table `article`
 --
 ALTER TABLE `article`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Contraintes pour les tables déchargées
